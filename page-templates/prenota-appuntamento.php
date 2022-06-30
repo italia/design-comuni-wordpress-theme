4c6a60ca450074122304a4efbe25bfeacab85c6f
scanavino.marcello <scanavino.marcello@accenture.com>
2022-06-30 15:42:53 +0200

    feat: template html prenotazione

---
+++ page-templates/prenota-appuntamento.php
@@ -0,0 +1,49 @@
+<?php
+/* Template Name: prenota-appuntamento
+ *
+ * Prenota appuntamento template file
+ *
+ * @package Design_Scuole_Italia
+ */
+global $post;
+get_header();
+
+?>
+	<main>
+		<?php
+		while ( have_posts() ) :
+			the_post();
+			?>
+            <div class="container" id="main-container">
+                <div class="row justify-content-center">
+                    <div class="col-12 col-lg-10">
+                        <?php get_template_part("template-parts/common/breadcrumb"); ?>
+                    </div>
+                </div>
+            </div>    
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-12 col-lg-10">
+                        <div class="cmp-heading pb-3 pb-lg-4">
+                            <h1><?php echo the_title(); ?></h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <?php get_template_part("template-parts/prenotazione/tabs"); ?>
+            </div>
+
+
+			<?php get_template_part("template-parts/common/assistenza-contatti"); ?>
+							
+		<?php 
+			endwhile; // End of the loop.
+		?>
+	</main>
+
+<?php
+get_footer();
+
+
+
---
+++ template-parts/prenotazione/tabs.php
@@ -0,0 +1,45 @@
+
+
+<div class="cmp-info-progress d-flex">
+    <!-- Desktop -->
+    <div class="info-progress-wrapper d-none d-lg-flex w-100 px-3 flex-column justify-content-end step-active">
+        <div class="info-progress-body d-flex justify-content-between align-self-end align-items-end w-100 py-3">
+            <span class="d-block h-100 title-medium text-uppercase">Luogo</span>
+        </div>
+    </div>
+    <!-- Desktop -->
+    <div class="info-progress-wrapper d-none d-lg-flex w-100 px-3 flex-column justify-content-end">
+        <div class="info-progress-body d-flex justify-content-between align-self-end align-items-end w-100 py-3">
+            <span class="d-block h-100 title-medium text-uppercase">Data e orario</span>
+        </div>
+    </div>
+    <!-- Desktop -->
+    <div class="info-progress-wrapper d-none d-lg-flex w-100 px-3 flex-column justify-content-end">
+        <div class="info-progress-body d-flex justify-content-between align-self-end align-items-end w-100 py-3">
+            <span class="d-block h-100 title-medium text-uppercase">Dettagli appuntamento</span>
+        </div>
+    </div>
+    <!-- Desktop -->
+    <div class="info-progress-wrapper d-none d-lg-flex w-100 px-3 flex-column justify-content-end">
+        <div class="info-progress-body d-flex justify-content-between align-self-end align-items-end w-100 py-3">
+            <span class="d-block h-100 title-medium text-uppercase">Richiedente</span>
+        </div>
+    </div>
+    <!-- Desktop -->
+    <div class="info-progress-wrapper d-none d-lg-flex w-100 px-3 flex-column justify-content-end">
+        <div class="info-progress-body d-flex justify-content-between align-self-end align-items-end w-100 py-3">
+            <span class="d-block h-100 title-medium text-uppercase">Riepilogo</span>
+        </div>
+    </div>
+
+    <!-- Mobile -->
+    <div class="iscrizioni-header d-lg-none w-100">
+        <h4 class="step-title d-flex align-items-center justify-content-between drop-shadow">
+            <span class="d-block d-lg-inline">
+            Luogo
+            </span>
+            <span class="step">1/5</span>
+        </h4>
+        <p class="title-xsmall mt-40 mb-3">I campi contraddistinti dal simbolo asterisco sono obbligatori</p>
+    </div>
+</div>
