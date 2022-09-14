0a075119733a929621561480eb012cc28416c908
Simon Rossi <s.rossi@xaos.it>
2022-09-14 11:16:23 +0200

    feat: background Sezione Evidenza Home compatibile con child theme

--- home.php
+++ home.php
@@ -15,7 +15,7 @@ get_header();
             <?php get_template_part("template-parts/home/calendario"); ?>
         </section>
         <section id="evidenza" class="evidence-section">
-            <div class="section section-background-header py-5" style="background-image: url('<?php echo esc_url( get_template_directory_uri()); ?>/assets/img/evidenza-header.png')">
+            <div class="section section-background-header py-5" style="<?php if (file_exists(get_stylesheet_directory().'/assets/img/evidenza-header.png')){ ?>background-image: url('<?php echo esc_url( get_stylesheet_directory_uri()); ?>/assets/img/evidenza-header.png');<?php }else{ ?>background-image: url('<?php echo esc_url( get_template_directory_uri()); ?>/assets/img/evidenza-header.png');<?php } ?>">
                 <?php get_template_part("template-parts/home/argomenti"); ?>
                 <?php get_template_part("template-parts/home/siti","tematici"); ?>
             </div>
