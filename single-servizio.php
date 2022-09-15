5207f4d274f7b6ca664681008625635396f6b6f8
Lorenzo Dalla Muta <lorenzo.dalla.muta@accenture.com>
2022-09-15 13:14:07 +0000

    fix: insert missing wrapper in details of servizio
    refs: 5270eb0

--- single-servizio.php
+++ single-servizio.php
@@ -250,138 +250,140 @@ get_header();
                         </div>
                     </div>
                     <div class="col-12 col-lg-8 offset-lg-1">
-                        <section class="mb-30">
-                            <h2 class="title-xxlarge mb-3" id="who-needs">A chi è rivolto</h2>
-                            <div class="richtext-wrapper lora">
-                                <?php echo $destinatari ?>
-                            </div>
-                        </section>
-                        <?php if ($descrizione) { ?>
-                        <section class="mb-30">
-                            <h2 class="title-xxlarge mb-3" id="description">Descrizione</h2>
-                            <div class="richtext-wrapper lora"><?php echo $descrizione ?></div>
-                        </section>
-                        <?php } ?>
-                        <section class="mb-30">
-                            <h2 class="title-xxlarge mb-3" id="how-to">Come fare</h2>
-                            <div class="richtext-wrapper lora">
-                                <?php echo $come_fare ?>
-                            </div>
-                        </section>
-                        <section class="mb-30 has-bg-grey p-3">
-                            <h2 class="title-xxlarge mb-3" id="needed">Cosa serve</h2>
-                            <div class="richtext-wrapper lora">
-                                <?php echo $cosa_serve_intro ?>
-                                <ul >
-                                    <?php foreach ($cosa_serve_list as $cosa_serve_item) { ?>
-                                        <li><span><?php echo $cosa_serve_item ?></span></li>
-                                    <?php } ?>
-                                </ul>
-                            </div>
-                        </section>
-                        <section class="mb-30">
-                            <h2 class="title-xxlarge mb-3" id="obtain">Cosa si ottiene</h2>
-                            <div class="richtext-wrapper lora"><?php echo $output ?></div>
-                        </section>
-                        <?php if ( is_array($fasi_scadenze) && count($fasi_scadenze) ) { ?>
-                        <section class="mb-30">
-                            <div class="cmp-timeline">
-                                <h2 class="title-xxlarge mb-3" id="deadlines">Tempi e scadenze</h2>
-                                <p class="text-paragraph mb-3 lora">
-                                    <?php echo $fasi_scadenze_intro; ?>
-                                </p>
-                                <div class="calendar-vertical mb-3">
-                                    <?php foreach ($fasi_scadenze as $fase_id) {
-                                        $fase = get_post($fase_id);
-                                        $data = dci_get_meta('data_fase', '_dci_fase_', $fase_id);
-                                        $arrdata =  explode("-", $data);
-                                        $monthName = date_i18n('M', mktime(0, 0, 0, $arrdata[1], 10)); // March
-                                        ?>
-                                        <div class="calendar-date">
-                                            <div class="calendar-date-day">
-                                                <small class="calendar-date-day__year"><?php echo $arrdata[2]; ?></small>
-                                                <span class="title-xxlarge-regular d-flex justify-content-center"><?php echo $arrdata[0]; ?></span>
-                                                <small class="calendar-date-day__month"><?php echo $monthName; ?></small>
-                                            </div>
-                                            <div class="calendar-date-description rounded">
-                                                <div class="calendar-date-description-content">
-                                                    <h3 class="title-medium-2 mb-0">
-                                                        <?php echo $fase->post_title; ?>
-                                                    </h3>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    <?php } ?>
+                        <div class="it-page-sections-container">
+                            <section class="it-page-section mb-30">
+                                <h2 class="title-xxlarge mb-3" id="who-needs">A chi è rivolto</h2>
+                                <div class="richtext-wrapper lora">
+                                    <?php echo $destinatari ?>
                                 </div>
-                            </div>
-                        </section>
-                        <?php } ?>
-                        <?php if ( $costi ) { ?>
-                        <section class="mb-30">
-                            <h2 class="title-xxlarge mb-3" id="costs">Quanto costa</h2>
-                            <div class="richtext-wrapper lora"><?php echo $costi ?></div>
-                        </section>
-                        <?php } ?>
-                        <section class="mb-30 has-bg-grey p-4">
-                            <h2 class="mb-3" id="submit-request">Accedi al servizio</h2>
-                            <?php if ($canale_digitale_link) { ?>
-                            <p class="text-paragraph lora mb-4"><?php echo $canale_digitale_text; ?></p>
-                            <button type="button" class="btn btn-primary mobile-full" onclick="location.href='<?php echo $canale_digitale_link; ?>';">
-                                <span class=""><?php echo $canale_digitale_label; ?></span>
-                            </button>
+                            </section>
+                            <?php if ($descrizione) { ?>
+                            <section class="it-page-section mb-30">
+                                <h2 class="title-xxlarge mb-3" id="description">Descrizione</h2>
+                                <div class="richtext-wrapper lora"><?php echo $descrizione ?></div>
+                            </section>
                             <?php } ?>
-                            <p class="text-paragraph lora mt-4"><?php echo $canale_fisico_text; ?></p>
-                            <button type="button" class="btn btn-outline-primary t-primary bg-white mobile-full" onclick="location.href='<?php echo dci_get_template_page_url('page-templates/prenota-appuntamento.php'); ?>';">
-                                <span class="">Prenota appuntamento</span>
-                            </button>
-                        </section>
-                        <?php if ( $more_info ) {  ?>
-                        <section class="mb-30">
-                            <h2 class="title-xxlarge mb-3" id="more-info">Ulteriori informazioni</h2>
-                            <h3 class="mb-3 subtitle-medium">Graduatorie di accesso</h3>
-                            <div class="richtext-wrapper lora">
-                                <?php echo $more_info ?>
-                            </div>
-                        </section>
-                        <?php }  ?>
-                        <?php if ( $condizioni_servizio ) {
-                            $file_url = $condizioni_servizio;
-                        ?>
-                        <section class="mb-30">
-                            <h2 class="title-xxlarge mb-3" id="conditions">Condizioni di servizio</h2>
-                            <div class="richtext-wrapper lora">Per conoscere i dettagli di
-                                scadenze, requisiti e altre informazioni importanti, leggi i termini e le condizioni di servizio.
-                            </div>
-                            <?php get_template_part("template-parts/single/attachment"); ?>
-                        </section>
-                        <?php } ?>
-
-                        <section class="it-page-section">
-                            <h2 class="mb-3" id="contacts">Contatti</h2>
-                            <div class="row">
-                                <div class="col-12 col-md-8 col-lg-6 mb-30">
-                                    <div class="card-wrapper rounded h-auto mt-10">
-                                        <?php
-                                            $with_border = true;
-                                            get_template_part("template-parts/unita-organizzativa/card");
-                                        ?>
-                                    </div>
+                            <section class="it-page-section mb-30">
+                                <h2 class="title-xxlarge mb-3" id="how-to">Come fare</h2>
+                                <div class="richtext-wrapper lora">
+                                    <?php echo $come_fare ?>
                                 </div>
-                                <div class="col-12 mb-30">
-                                    <span class="text-paragraph-small">Argomenti:</span>
-                                    <div class="d-flex flex-wrap gap-2 mt-10 mb-30">
-                                        <?php foreach ( $argomenti as $item ) { ?>
-                                            <div class="cmp-tag">
-                                                <a class="chip chip-simple t-primary bg-tag text-decoration-none" href="<?php echo get_term_link($item); ?>" data-element="service-topic">
-                                                <span class="chip-label"><?php echo $item->name; ?></span>
-                                                </a>
+                            </section>
+                            <section class="it-page-section mb-30 has-bg-grey p-3">
+                                <h2 class="title-xxlarge mb-3" id="needed">Cosa serve</h2>
+                                <div class="richtext-wrapper lora">
+                                    <?php echo $cosa_serve_intro ?>
+                                    <ul >
+                                        <?php foreach ($cosa_serve_list as $cosa_serve_item) { ?>
+                                            <li><span><?php echo $cosa_serve_item ?></span></li>
+                                        <?php } ?>
+                                    </ul>
+                                </div>
+                            </section>
+                            <section class="it-page-section mb-30">
+                                <h2 class="title-xxlarge mb-3" id="obtain">Cosa si ottiene</h2>
+                                <div class="richtext-wrapper lora"><?php echo $output ?></div>
+                            </section>
+                            <?php if ( is_array($fasi_scadenze) && count($fasi_scadenze) ) { ?>
+                            <section class="it-page-section mb-30">
+                                <div class="cmp-timeline">
+                                    <h2 class="title-xxlarge mb-3" id="deadlines">Tempi e scadenze</h2>
+                                    <p class="text-paragraph mb-3 lora">
+                                        <?php echo $fasi_scadenze_intro; ?>
+                                    </p>
+                                    <div class="calendar-vertical mb-3">
+                                        <?php foreach ($fasi_scadenze as $fase_id) {
+                                            $fase = get_post($fase_id);
+                                            $data = dci_get_meta('data_fase', '_dci_fase_', $fase_id);
+                                            $arrdata =  explode("-", $data);
+                                            $monthName = date_i18n('M', mktime(0, 0, 0, $arrdata[1], 10)); // March
+                                            ?>
+                                            <div class="calendar-date">
+                                                <div class="calendar-date-day">
+                                                    <small class="calendar-date-day__year"><?php echo $arrdata[2]; ?></small>
+                                                    <span class="title-xxlarge-regular d-flex justify-content-center"><?php echo $arrdata[0]; ?></span>
+                                                    <small class="calendar-date-day__month"><?php echo $monthName; ?></small>
+                                                </div>
+                                                <div class="calendar-date-description rounded">
+                                                    <div class="calendar-date-description-content">
+                                                        <h3 class="title-medium-2 mb-0">
+                                                            <?php echo $fase->post_title; ?>
+                                                        </h3>
+                                                    </div>
+                                                </div>
                                             </div>
                                         <?php } ?>
                                     </div>
-                                    <?php get_template_part('template-parts/single/page_bottom',"simple"); ?>
                                 </div>
-                            </div>
-                        </section>
+                            </section>
+                            <?php } ?>
+                            <?php if ( $costi ) { ?>
+                            <section class="it-page-section mb-30">
+                                <h2 class="title-xxlarge mb-3" id="costs">Quanto costa</h2>
+                                <div class="richtext-wrapper lora"><?php echo $costi ?></div>
+                            </section>
+                            <?php } ?>
+                            <section class="it-page-section mb-30 has-bg-grey p-4">
+                                <h2 class="mb-3" id="submit-request">Accedi al servizio</h2>
+                                <?php if ($canale_digitale_link) { ?>
+                                <p class="text-paragraph lora mb-4"><?php echo $canale_digitale_text; ?></p>
+                                <button type="button" class="btn btn-primary mobile-full" onclick="location.href='<?php echo $canale_digitale_link; ?>';">
+                                    <span class=""><?php echo $canale_digitale_label; ?></span>
+                                </button>
+                                <?php } ?>
+                                <p class="text-paragraph lora mt-4"><?php echo $canale_fisico_text; ?></p>
+                                <button type="button" class="btn btn-outline-primary t-primary bg-white mobile-full" onclick="location.href='<?php echo dci_get_template_page_url('page-templates/prenota-appuntamento.php'); ?>';">
+                                    <span class="">Prenota appuntamento</span>
+                                </button>
+                            </section>
+                            <?php if ( $more_info ) {  ?>
+                            <section class="it-page-section mb-30">
+                                <h2 class="title-xxlarge mb-3" id="more-info">Ulteriori informazioni</h2>
+                                <h3 class="mb-3 subtitle-medium">Graduatorie di accesso</h3>
+                                <div class="richtext-wrapper lora">
+                                    <?php echo $more_info ?>
+                                </div>
+                            </section>
+                            <?php }  ?>
+                            <?php if ( $condizioni_servizio ) {
+                                $file_url = $condizioni_servizio;
+                            ?>
+                            <section class="it-page-section mb-30">
+                                <h2 class="title-xxlarge mb-3" id="conditions">Condizioni di servizio</h2>
+                                <div class="richtext-wrapper lora">Per conoscere i dettagli di
+                                    scadenze, requisiti e altre informazioni importanti, leggi i termini e le condizioni di servizio.
+                                </div>
+                                <?php get_template_part("template-parts/single/attachment"); ?>
+                            </section>
+                            <?php } ?>
+
+                            <section class="it-page-section">
+                                <h2 class="mb-3" id="contacts">Contatti</h2>
+                                <div class="row">
+                                    <div class="col-12 col-md-8 col-lg-6 mb-30">
+                                        <div class="card-wrapper rounded h-auto mt-10">
+                                            <?php
+                                                $with_border = true;
+                                                get_template_part("template-parts/unita-organizzativa/card");
+                                            ?>
+                                        </div>
+                                    </div>
+                                    <div class="col-12 mb-30">
+                                        <span class="text-paragraph-small">Argomenti:</span>
+                                        <div class="d-flex flex-wrap gap-2 mt-10 mb-30">
+                                            <?php foreach ( $argomenti as $item ) { ?>
+                                                <div class="cmp-tag">
+                                                    <a class="chip chip-simple t-primary bg-tag text-decoration-none" href="<?php echo get_term_link($item); ?>" data-element="service-topic">
+                                                    <span class="chip-label"><?php echo $item->name; ?></span>
+                                                    </a>
+                                                </div>
+                                            <?php } ?>
+                                        </div>
+                                        <?php get_template_part('template-parts/single/page_bottom',"simple"); ?>
+                                    </div>
+                                </div>
+                            </section>
+                        </div>
                     </div>
                 </div>
             </div>
