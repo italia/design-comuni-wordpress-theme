47488b9c3364d4ea88bf8f026f19a31bb148929b
Lorenzo Dalla Muta <lorenzo.dalla.muta@accenture.com>
2022-10-03 14:23:56 +0000

    feat: align slimheader markup to templates

--- template-parts/header/header-anon.php
+++ template-parts/header/header-anon.php
@@ -1,8 +1,8 @@
 <a class="btn btn-primary btn-icon btn-full" href="#" data-element="personal-area-login">
-    <span class="rounded-icon">
-    <svg class="icon icon-primary">
-        <use xlink:href="#it-user"></use>
-    </svg>
+    <span class="rounded-icon" aria-hidden="true">
+        <svg class="icon icon-primary">
+            <use xlink:href="#it-user"></use>
+        </svg>
     </span>
     <span class="d-none d-lg-block">Accedi all'area personale</span>
 </a>
--- template-parts/header/slimheader.php
+++ template-parts/header/slimheader.php
@@ -1,55 +1,49 @@
 <div class="it-header-slim-wrapper">
-    <div class="container">
-      <div class="row">
-        <div class="col-12">
-          <div class="it-header-slim-wrapper-content">
-            <a class="d-lg-block navbar-brand" href="<?php echo dci_get_option("url_sito_regione"); ?>" target="_blank" aria-label="Vai al portale <?php echo dci_get_option("nome_regione"); ?> - link esterno - apertura nuova scheda" title="Vai al portale <?php echo dci_get_option("nome_regione"); ?>"><?php echo dci_get_option("nome_regione"); ?></a>
-            <div class="it-header-slim-right-zone">
-              <div class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">
-                  <span class="visually-hidden">Selezione lingua: lingua selezionata</span>
-                  <span>ITA</span>
-                  <svg class="icon d-none d-lg-block">
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="it-header-slim-wrapper-content">
+          <a class="d-lg-block navbar-brand" target="_blank" href="<?php echo dci_get_option("url_sito_regione"); ?>" target="_blank" aria-label="Vai al portale <?php echo dci_get_option("nome_regione"); ?> - link esterno - apertura nuova scheda" title="Vai al portale <?php echo dci_get_option("nome_regione"); ?>"><?php echo dci_get_option("nome_regione"); ?></a>
+          <div class="it-header-slim-right-zone" role="navigation">
+            <div class="nav-item dropdown">
+              <button type="button" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" aria-controls="languages" aria-haspopup="true">
+                <span class="visually-hidden">Lingua attiva:</span>
+                <span>ITA</span>
+                <svg class="icon">
                     <use href="#it-expand"></use>
                   </svg>
-                </a>
-                <div class="dropdown-menu">
-                  <div class="row">
-                    <div class="col-12">
-                      <div class="link-list-wrapper">
-                        <ul class="link-list">
-                          <li>
-                            <a class="dropdown-item list-item" href="#"
-                              ><span
-                                >ITA
-                                <span class="visually-hidden"
-                                  >selezionata</span
-                                ></span
-                              ></a
-                            >
-                          </li>
-                          <li>
-                            <a class="dropdown-item list-item" href="#"
-                              ><span>ENG</span></a
-                            >
-                          </li>
-                        </ul>
-                      </div>
+              </button>
+              <div class="dropdown-menu">
+                <div class="row">
+                  <div class="col-12">
+                    <div class="link-list-wrapper">
+                      <ul class="link-list">
+                        <li>
+                          <a class="dropdown-item list-item" href="#">
+                            <span>ITA<span class="visually-hidden">selezionata</span></span>
+                          </a>
+                        </li>
+                        <li>
+                          <a class="dropdown-item list-item" href="#">
+                            <span>ENG</span>
+                          </a>
+                        </li>
+                      </ul>
                     </div>
                   </div>
                 </div>
               </div>
-              <?php
+            </div>
+            <?php
                 if(!is_user_logged_in()) {
                     get_template_part("template-parts/header/header-anon");
                 }else{
                     get_template_part("template-parts/header/header-logged");
                 }
               ?>
-            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-  
+</div>
