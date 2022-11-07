a6558c89604f60ffad188b690b25c9f41a33257c
marcelloscanavino <90757415+marcelloscanavino@users.noreply.github.com>
2022-11-07 12:50:40 +0100

    fix: update template argomento con colonne bootstrap

--- template-parts/argomento/amministrazione-detail.php
+++ template-parts/argomento/amministrazione-detail.php
@@ -8,48 +8,49 @@
         <div class="container">
             <div class="row row-title">
                 <div class="col-12">
-                    <h3 class="u-grey-light border-bottom border-semi-dark pb-2 pb-lg-3 title-large-semi-bold">
-                        Amministrazione
+                    <h3 class="u-grey-light border-bottom border-semi-dark pb-2 pb-lg-3 title-large-semi-bold">Amministrazione
                     </h3>
                 </div>
             </div>
-        <div>
-            <div class="card-wrapper card-teaser-wrapper card-teaser-wrapper-equal card-teaser-block-3 py-2 py-lg-3 d-flex d-md-block d-lg-flex">
+            <div class="row mt-0 g-4">
                 <?php foreach ($posts as $post) { 
                     $description = dci_get_meta('descrizione_breve');
                     $img = dci_get_meta('immagine');    
                     $tipo_amministrazione = get_the_terms($post->ID, 'tipi_unita_organizzativa')[0];
                 ?>
-                    <div class="card card-teaser rounded border border-light <?php echo $img ?'card-teaser-image card-flex' : '' ?> ">
-                        <div class="card-body d-flex justify-content-between">
-                            <div class="py-4 ps-4">
+                    <div class="col-lg-4">
+                        <div class="card-teaser card-teaser-image card-flex rounded border border-light">
+                            <div class="card-body d-flex justify-content-between">
+                            <div class="teaser-content">
                                 <div class="category-top">
-                                    <a class="text-decoration-none fw-semibold" href="<?php echo get_term_link($tipo_amministrazione->term_id); ?>">
-                                <?php echo $tipo_amministrazione->name; ?>
-                            </a>
+                                    <a class="text-decoration-none fw-semibold" href="<?php echo get_term_link($tipo_amministrazione->term_id); ?>"><?php echo $tipo_amministrazione->name; ?></a>
                                 </div>
-                                <h4 class="text-paragraph-medium">
-                                    <a class="text-decoration-none" href="<?php echo get_permalink(); ?>"><?php echo the_title(); ?></a>
-                                </h4>
-                                <p class="m-0 u-grey-light text-paragraph-card">
-                                    <?php echo $description; ?>
-                                </p>
+                                    <h4 class="text-paragraph-medium">
+                                        <a href="<?php echo get_permalink(); ?>" class="text-decoration-none"><?php echo the_title(); ?></a>
+                                    </h4>
+                                <p class="m-0 u-grey-light text-paragraph-card"><?php echo $description; ?></p>
                             </div>
-                            <div class="card-image card-image-rounded card-bg-image" style='background-image: url("<?php echo $img; ?>");'></div>
-                        </div>
+                            <?php if ($img) { ?>
+                                <div class="card-image">
+                                    <?php dci_get_img($img); ?>
+                                </div>
+                            <?php } ?>
+                            </div>
+                        </div>          
                     </div>
                 <?php } ?>
             </div>
-            <div class="col-12 col-lg-3 offset-lg-9">
-            <button 
-                type="button" 
-                class="btn btn-primary text-button w-100"
-                onclick="location.href='<?php echo dci_get_template_page_url('page-templates/amministrazione.php'); ?>'"
-            >
-                Tutta l’amministrazione
-            </button>
+            <div class="row mt-4">
+                <div class="col-12 col-lg-3 offset-lg-9">
+                <button 
+                    type="button" 
+                    class="btn btn-primary text-button w-100"
+                    onclick="location.href='<?php echo dci_get_template_page_url('page-templates/amministrazione.php'); ?>'"
+                >
+                    Tutta l’amministrazione
+                </button>
+                </div>
             </div>
-        </div>
         </div>
     </div>
 </section>
--- template-parts/argomento/servizi-detail.php
+++ template-parts/argomento/servizi-detail.php
@@ -14,39 +14,39 @@
                     </h3>
                 </div>
             </div>
-        <div>
-            <div class="card-wrapper card-teaser-wrapper card-teaser-wrapper-equal card-teaser-block-3 py-2 py-lg-3 justify-content-between">
+            <div class="row mt-0 g-4">
                 <?php foreach ($posts as $post) { 
                     $description = dci_get_meta('descrizione_breve');
                     $categoria_servizio = get_the_terms($post->ID, 'categorie_servizio')[0];
                 ?>
-                    <div class="card card-teaser rounded border border-light">
-                        <div class="card-body">
-                            <div class="category-top">
-                                <a class="text-decoration-none fw-semibold" href="<?php echo get_term_link($categoria_servizio->term_id); ?>">
-                                    <?php echo $categoria_servizio->name; ?>
-                                </a>
+                    <div class="col-lg-4">
+                        <div class="card-teaser card-teaser-image card-flex rounded border border-light">
+                            <div class="card-body d-flex justify-content-between">
+                                <div class="teaser-content">
+                                    <div class="category-top">
+                                        <a class="text-decoration-none fw-semibold" href="<?php echo get_term_link($categoria_servizio->term_id); ?>"><?php echo $categoria_servizio->name; ?></a>
+                                    </div>
+                                        <h4 class="text-paragraph-medium">
+                                            <a href="<?php echo get_permalink(); ?>" class="text-decoration-none"><?php echo the_title(); ?></a>
+                                        </h4>
+                                    <p class="m-0 u-grey-light text-paragraph-card"><?php echo $description; ?></p>
+                                </div>
                             </div>
-                            <h4 class="text-paragraph-medium ">
-                                <a class="text-decoration-none" href="<?php echo get_permalink(); ?>"><?php echo the_title(); ?></a>  
-                            </h4>
-                            <p class="m-0 u-grey-light text-paragraph-card">
-                                <?php echo $description; ?>
-                            </p>
-                        </div>
+                        </div>          
                     </div>
                 <?php } ?>
             </div>
-            <div class="col-12 col-lg-3 offset-lg-9">
-            <button 
-                type="button" 
-                class="btn btn-primary text-button w-100"
-                onclick="location.href='<?php echo dci_get_template_page_url('page-templates/servizi.php'); ?>'"
-            >
-                Tutti i servizi
-            </button>
+            <div class="row mt-4">
+                <div class="col-12 col-lg-3 offset-lg-9">
+                    <button 
+                        type="button" 
+                        class="btn btn-primary text-button w-100"
+                        onclick="location.href='<?php echo dci_get_template_page_url('page-templates/servizi.php'); ?>'"
+                    >
+                        Tutti i servizi
+                    </button>
+                </div>
             </div>
-        </div>
         </div>
     </div>
 </section>
