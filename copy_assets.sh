3f6b453667d76853e7dbe34882b1aaf3878661c2
Andrea Stagi <stagi.andrea@gmail.com>
2022-11-09 11:57:31 +0100

    chore(deps): upgrade bootstrap italia

---
+++ copy_assets.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp ./node_modules/bootstrap-italia/dist/css/bootstrap-italia-comuni.min.css ./assets/css/bootstrap-italia.min.css
+cp ./node_modules/bootstrap-italia/dist/js/bootstrap-italia.bundle.min.js ./assets/js/bootstrap-italia.bundle.min.js
