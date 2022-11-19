23972c65674178ac0e0ec1b58d631595a89e1f66
giannicozzolino <gianni.cozzolino@gmail.com>
2022-11-19 16:52:58 +0100

    Scheda Unità Organizzativa Fixes #29

--- template-parts/persona/card-vertical-thumb.php
+++ template-parts/persona/card-vertical-thumb.php
@@ -8,7 +8,6 @@ $organizzazioni_id = dci_get_meta('organizzazioni', $prefix, $persona_id);
 $incarichi_id = dci_get_meta('incarichi', $prefix, $persona_id);
 
 ?>
-<<<<<<< HEAD
 <div class="card no-after shadow-sm  rounded">
     <div class="row g-0">
         <div class="col-md-8 px-4 pt-4 pb-4">
@@ -41,27 +40,6 @@ $incarichi_id = dci_get_meta('incarichi', $prefix, $persona_id);
                     }
                     ?>
                 </p>
-=======
-<div class="card no-after card-bg card-vertical-thumb bg-white">
-    <div class="row g-0">
-        <div class="col-md-8">
-            <div class="card-body">
-                <?php if ($organizzazioni_id) { ?>
-                    <h5 class="card-title">
-                        <?php
-                        foreach ($organizzazioni_id as $org_id) {
-                            $organizzazione = get_post($org_id);
-                            echo '<a href="' . get_the_permalink($org_id) . '">' . get_the_title($org_id) . '</a>';
-                        } ?>
-                    </h5>
-                <?php } ?>
-                <h4><a href="<?php echo get_permalink($persona); ?>"><?php echo get_the_title($persona); ?></a></h4>
-                <?php if ($incarichi_id) {
-                    foreach ($incarichi_id as $inc_id) {
-                        echo get_the_title($inc_id) . ' - ';
-                    }
-                } ?>
->>>>>>> c952751a67506647e482acc34a3bbcd57f0a1017
             </div>
         </div>
         <div class="col">
