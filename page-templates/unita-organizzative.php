44404525f249c4a28e036552633d9d1f0a517bf2
Giovanni Cozzolino <gianni.cozzolino@gmail.com>
2022-12-04 20:37:22 +0100

    Added the "Organizational Unit" page-template to display the list of UOs into the "Amministrazione" section

---
+++ page-templates/unita-organizzative.php
@@ -0,0 +1,32 @@
+<?php
+/* Template Name: Unità Organizzative
+ *
+ * Servizi template file
+ *
+ * @package Design_Comuni_Italia
+ */
+global $post;
+get_header();
+
+?>
+	<main>
+		<?php
+		while ( have_posts() ) :
+			the_post();
+			?>
+			<?php get_template_part("template-parts/hero/hero"); ?>
+			<?php get_template_part("template-parts/unita-organizzativa/tutte-uo"); ?>
+<!--			<?php //get_template_part("template-parts/servizio/categorie"); ?>  -->
+			<?php get_template_part("template-parts/common/valuta-servizio"); ?>
+			<?php get_template_part("template-parts/common/assistenza-contatti"); ?>
+							
+		<?php 
+			endwhile; // End of the loop.
+		?>
+	</main>
+
+<?php
+get_footer();
+
+
+
