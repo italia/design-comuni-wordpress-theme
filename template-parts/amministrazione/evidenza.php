58c8042702f5b5d00b409681c54ba43d9b382800
Lorenzo Dalla Muta <lorenzo.dalla.muta@accenture.com>
2023-01-16 15:39:01 +0000

    feat: data element for all cards in amministrazione

--- template-parts/amministrazione/evidenza.php
+++ template-parts/amministrazione/evidenza.php
@@ -22,7 +22,9 @@
                 <div class="cmp-card-simple card-wrapper pb-0 rounded border border-light">
                     <div class="card shadow-sm rounded">
                         <div class="card-body">
-                            <a class="text-decoration-none" href="<?php echo get_permalink($post->ID); ?>" data-element="administration-element">
+                            <a class="text-decoration-none" href="<?php echo get_permalink($post->ID); ?>"
+                                data-element="management-category-link"
+                            >
                                 <h3 class="card-title t-primary title-xlarge">
                                     <?php echo $post->post_title; ?>
                                 </h3>
