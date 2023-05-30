6b589235b488e6ae11ebb5ed8e781fbd2dc0a9fc
giannicozzolino <77513351+giannicozzolino@users.noreply.github.com>
2023-05-30 08:00:43 +0200

    Modificata la struttura delle card documento pubblico

--- template-parts/documento/card.php
+++ template-parts/documento/card.php
@@ -1,20 +1,77 @@
 <?php
-global $documento;
+global $post_id, $excerpt;
+
+$post = get_post( $post_id );
+
+$description = dci_get_meta( 'descrizione_breve' );
+if ( $post->post_type == 'documento_pubblico' ) {
+	$ufficio_id = dci_get_meta( 'ufficio_responsabile', '_dci_documento_pubblico_', $post->ID )[0] ?? '';
+	$ufficio    = get_post( $ufficio_id );
+
+	$url_documento  = dci_get_meta( 'url_documento', '_dci_documento_pubblico_', $post->ID ) ?? '';
+	$file_documento = dci_get_meta( 'file_documento', '_dci_documento_pubblico_', $post->ID );
+	$link_documento = ( $url_documento != '' ) ? $url_documento : $file_documento;
+	//var_dump($link_documento);
+}
+
+var_dump(dci_get_meta( "data_protocollo" ) );
+
+if ( $post->post_type == 'dataset' ) {
+	$tipo    = '';
+	$arrdata = explode( '-', date( 'd-m-Y', dci_get_meta( "data_modifica" ) ) );
+} else {
+	$arrdata = explode( '-', dci_get_meta( "data_protocollo" ) );
+	$tipo    = get_the_terms( $post_id, 'tipi_doc_albo_pretorio' )[0] ?? get_the_terms( $post_id, 'tipi_documento' )[0];
+}
+$monthName = date_i18n( 'M', mktime( 0, 0, 0, $arrdata[1], 10 ) );
+$img       = dci_get_meta( 'immagine' );
 ?>
-<div class="card card-teaser shadow-sm p-4s rounded border border-light flex-nowrap m-1">
-	<svg class="icon" aria-hidden="true">
-		<use xlink:href="#it-clip"></use>
-	</svg>
-	<div class="card-body">
-		<h5 class="card-title">
-			<a href="<?php echo get_permalink($documento->ID); ?>">
-				<?php echo $documento->post_title; ?>
-			</a>
-		</h5>
-		<div class="card-text">
-			<p>
-				<?php echo dci_get_meta('descrizione_breve','_dci_documento_pubblico_',$documento->ID); ?>
-			</p>
+
+
+<div class="card no-after rounded shadow-sm h-100">
+	<div class="row g-2">
+		<?php if ( $img ) { ?>
+			<div style="max-width: 150px;" class="col-auto h-100 p-2 order-1 d-flex flex-wrap align-items-center">
+				<?php dci_get_img( $img, 'rounded-top img-fluid img-responsive' ); ?>
+			</div>
+		<?php } ?>
+
+		<div class="col order-2">
+			<div class="card-body">
+				<div class="g-2">
+					<div class="float-end">
+						<a class="button button-link" href="<?= $link_documento ?>">
+							<svg style="font-weight: bold;" fill="darkgreen" class="icon" viewBox="0 0 20 20">
+								<use xlink:href="#it-download"></use>
+							</svg>
+						</a>
+					</div>
+					<div>
+						<div class="category-top cmp-list-card-img__body">
+							<?php if ( $tipo ) { ?>
+								<span
+									class="category cmp-list-card-img__body-heading-title underline"><?php echo $tipo->name ? $tipo->name : 'DATASET'; ?></span>
+							<?php } ?>
+							<span class="data"><?php echo $arrdata[0] . ' ' . $monthName . ' ' . $arrdata[2] ?></span>
+						</div>
+						<a class="text-decoration-none" href="<?php echo get_permalink(); ?>">
+							<h3 class="h5 card-title">
+								<?php
+								if ( ! $excerpt ?? null ) {
+									echo 'VERO';
+									echo the_title();
+								} else {
+									echo wp_trim_words( get_the_title( $post_id ), 30 );
+								}
+								?>
+							</h3>
+						</a>
+						<p class="card-text d-none d-md-block">
+							<?php echo $description; ?>
+						</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
--- template-parts/documento/tutti-documenti.php
+++ template-parts/documento/tutti-documenti.php
@@ -1,41 +1,44 @@
 <?php
-global $the_query, $load_posts, $load_card_type, $tipo_documento;
+global $the_query, $load_posts, $load_card_type, $tipo_documento, $post_id, $excerpt;
 
-$max_posts = $_GET['max_posts'] ?? 50;
+$max_posts  = $_GET['max_posts'] ?? 50;
 $load_posts = 50;
-$query = $_GET['search'] ?? '';
-
+$query      = $_GET['search'] ?? '';
 
 
 $orderBy = 'data_modifica';
-$order = 'DESC';
+$order   = 'DESC';
 
-if ($tipo_documento ?? null) {
-    if ($tipo_documento->taxonomy == 'documento_pubblico' || $tipo_documento->taxonomy == 'documento') {
-        $orderBy = 'data_protocollo';
-    }
-    $args = array(
-        's'              => $query,
-        'posts_per_page' => $max_posts,
-        'post_type'      => array('documento_pubblico', 'documento', 'dataset'),
-        'orderby'        => $orderBy,
-        'order'          => $order,
-        'tax_query' => array(
-            array (
-                'taxonomy' => $tipo_documento->taxonomy,
-                'field' => 'term_id',
-                'terms' => $tipo_documento->term_id,
-            )
-        ),
-    );
+if ( $tipo_documento ?? null ) {
+	if ( $tipo_documento->taxonomy == 'documento_pubblico' || $tipo_documento->taxonomy == 'documento' ) {
+		$orderBy = 'data_protocollo';
+	}
+	$args = array(
+		's'              => $query,
+		'posts_per_page' => $max_posts,
+		'post_type'      => array( 'documento_pubblico', 'documento', 'dataset' ),
+		'fields'         => 'ids',
+		'status'         => 'publish',
+		'orderby'        => $orderBy,
+		'order'          => $order,
+		'tax_query'      => array(
+			array(
+				'taxonomy' => $tipo_documento->taxonomy,
+				'field'    => 'term_id',
+				'terms'    => $tipo_documento->term_id,
+			)
+		),
+	);
 } else {
-    $args = array(
-        's'              => $query,
-        'posts_per_page' => $max_posts,
-        'post_type'      => array( 'documento_pubblico', 'documento', 'dataset' ),
-        'orderby'        => $orderBy,
-        'order'          => $order,
-    );
+	$args = array(
+		's'              => $query,
+		'posts_per_page' => $max_posts,
+		'post_type'      => array( 'documento_pubblico', 'documento', 'dataset' ),
+		'fields'         => 'ids',
+		'status'         => 'publish',
+		'orderby'        => $orderBy,
+		'order'          => $order,
+	);
 }
 $the_query = new WP_Query( $args );
 
@@ -44,59 +47,61 @@ $posts = $the_query->posts;
 
 
 <div class="bg-grey-card py-5">
-    <form role="search" id="search-form" method="get" class="search-form">
-    <button type="submit" class="d-none"></button>
-        <div class="container">
-            <h2 class="title-xxlarge mb-4">
-                Esplora i documenti
-            </h2>
-            <div>
-                <div class="cmp-input-search">
-                    <div class="form-group autocomplete-wrapper mb-0">
-                        <div class="input-group">
-                        <label for="autocomplete-two" class="visually-hidden"
-                        >Cerca</label
-                        >
-                        <input
-                        type="search"
-                        class="autocomplete form-control"
-                        placeholder="Cerca per parola chiave"
-                        id="autocomplete-two"
-                        name="search"
-                        value="<?php echo $query; ?>"
-                        data-bs-autocomplete="[]"
-                        />
-                        <div class="input-group-append">
-                            <button class="btn btn-primary" type="submit" id="button-3">
-                                Invio
-                            </button>
-                        </div>
-                        <span class="autocomplete-icon" aria-hidden="true"
-                        ><svg
-                            class="icon icon-sm icon-primary"
-                            role="img"
-                            aria-labelledby="autocomplete-label"
-                        >
+	<form role="search" id="search-form" method="get" class="search-form">
+		<button type="submit" class="d-none"></button>
+		<div class="container">
+			<h2 class="title-xxlarge mb-4">
+				Esplora i documenti
+			</h2>
+			<div>
+				<div class="cmp-input-search">
+					<div class="form-group autocomplete-wrapper mb-0">
+						<div class="input-group">
+							<label for="autocomplete-two" class="visually-hidden"
+							>Cerca</label
+							>
+							<input
+								type="search"
+								class="autocomplete form-control"
+								placeholder="Cerca per parola chiave"
+								id="autocomplete-two"
+								name="search"
+								value="<?php echo $query; ?>"
+								data-bs-autocomplete="[]"
+							/>
+							<div class="input-group-append">
+								<button class="btn btn-primary" type="submit" id="button-3">
+									Invio
+								</button>
+							</div>
+							<span class="autocomplete-icon" aria-hidden="true"
+							><svg
+									class="icon icon-sm icon-primary"
+									role="img"
+									aria-labelledby="autocomplete-label"
+								>
                             <use
-                            href="#it-search"
+	                            href="#it-search"
                             ></use></svg></span>
-                        </div>
-                        <p
-                        id="autocomplete-label"
-                        class="u-grey-light text-paragraph-card mt-2 mb-4 mt-lg-3 mb-lg-40"
-                        >
-                        <?php echo $the_query->found_posts; ?> documenti trovati in ordine cronologico decrescente
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="row g-4" id="load-more">
-                <?php 
-                    $load_card_type = 'documento';
-                    foreach ($posts as $post) {get_template_part('template-parts/documento/cards-list');
-                }?>
-            </div>
-            <?php get_template_part("template-parts/search/more-results"); ?>
-        </div>
-    </form>
+						</div>
+						<p id="autocomplete-label" class="u-grey-light text-paragraph-card mt-2 mb-4 mt-lg-3 mb-lg-40">
+							<?php echo $the_query->found_posts; ?> documenti trovati in ordine cronologico decrescente
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="row g-2" id="load-more">
+				<?php
+				$load_card_type = 'documento';
+				$excerpt = true;
+				foreach ( $posts as $post_id ) { ?>
+					<div class="col-lg-4">
+					<?php get_template_part( 'template-parts/documento/card' ); ?>
+					</div>
+				<?php }
+				?>
+			</div>
+			<?php get_template_part( "template-parts/search/more-results" ); ?>
+		</div>
+	</form>
 </div>
