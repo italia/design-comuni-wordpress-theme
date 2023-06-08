024200821efcd9a06c7a84e16c9c6d91a56d4767
Francesco Improta <francesco@designabile.com>
2023-06-08 12:08:24 +0200

    Beautify

--- template-parts/novita/tutte-novita.php
+++ template-parts/novita/tutte-novita.php
@@ -29,7 +29,7 @@ global $the_query, $load_posts, $load_card_type;
 
 <div class="bg-grey-card py-5">
     <form role="search" id="search-form" method="get" class="search-form">
-    <button type="submit" class="d-none"></button>
+        <button type="submit" class="d-none"></button>
         <div class="container">
             <h2 class="title-xxlarge mb-4">
                 Esplora tutte le novità
@@ -38,39 +38,23 @@ global $the_query, $load_posts, $load_card_type;
                 <div class="cmp-input-search">
                     <div class="form-group autocomplete-wrapper mb-0">
                         <div class="input-group">
-                            <label for="autocomplete-two" class="visually-hidden"
-                            >Cerca</label
-                            >
-                            <input
-                            type="search"
-                            class="autocomplete form-control"
-                            placeholder="Cerca per parola chiave"
-                            id="autocomplete-two"
-                            name="search"
-                            value="<?php echo $query; ?>"
-                            data-bs-autocomplete="[]"
-                            />
+                            <label for="autocomplete-two" class="visually-hidden">Cerca</label>
+                            <input type="search" class="autocomplete form-control" placeholder="Cerca per parola chiave"
+                                id="autocomplete-two" name="search" value="<?php echo $query; ?>"
+                                data-bs-autocomplete="[]" />
                             <div class="input-group-append">
                                 <button class="btn btn-primary" type="submit" id="button-3">
                                     Invio
                                 </button>
                             </div>
-                            <span class="autocomplete-icon" aria-hidden="true"
-                            ><svg
-                                class="icon icon-sm icon-primary"
-                                role="img"
-                                aria-labelledby="autocomplete-label"
-                            >
-                                <use
-                                href="#it-search"
-                                ></use></svg>
+                            <span class="autocomplete-icon" aria-hidden="true"><svg class="icon icon-sm icon-primary"
+                                    role="img" aria-labelledby="autocomplete-label">
+                                    <use href="#it-search"></use>
+                                </svg>
                             </span>
                         </div>
-                        <p
-                        id="autocomplete-label"
-                        class="u-grey-light text-paragraph-card mt-2 mb-30 mt-lg-3 mb-lg-40"
-                        >
-                        <?php echo $the_query->found_posts; ?> notizie trovate in ordine alfabetico
+                        <p id="autocomplete-label" class="u-grey-light text-paragraph-card mt-2 mb-30 mt-lg-3 mb-lg-40">
+                            <?php echo $the_query->found_posts; ?> notizie trovate in ordine alfabetico
                         </p>
                     </div>
                 </div>
