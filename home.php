bdef818ce7d82eae140102cb5a142a371ec299df
Francesco Improta <francesco@designabile.com>
2023-06-13 11:41:09 +0200

    Fie homepage classes

--- home.php
+++ home.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * The template for displaying home
  *
@@ -10,31 +9,30 @@
 
 get_header();
 ?>
-<main id="main-container" class="main-container redbrown">
-    <h1 class="visually-hidden">
-        <?php echo dci_get_option("nome_comune"); ?>
-    </h1>
-    <section id="evidenza" class="evidence-section">
-        <div class="section-content">
+    <main id="main-container" class="main-container redbrown">
+        <h1 class="visually-hidden" id="main-container">
+            <?php echo dci_get_option("nome_comune"); ?>
+        </h1>
+        <section id="head-section">
             <h2 class="visually-hidden">Contenuti in evidenza</h2>
             <?php
-            $messages = dci_get_option("messages", "home_messages");
-            if ($messages && !empty($messages)) {
+			$messages = dci_get_option( "messages", "home_messages" );
+            if($messages && !empty($messages)) {
                 get_template_part("template-parts/home/messages");
             }
-            ?>
+		    ?>
             <?php get_template_part("template-parts/home/notizie"); ?>
             <?php get_template_part("template-parts/home/calendario"); ?>
-            <div class="py-5 pb-lg-80 px-lg-5 position-relative" style="<?php if (file_exists(get_stylesheet_directory() . '/assets/img/evidenza-header.png')) { ?>background-image: url('<?php echo esc_url(get_stylesheet_directory_uri()); ?>/assets/img/evidenza-header.png');<?php } else { ?>background-image: url('<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/evidenza-header.png');<?php } ?>">
+        </section>
+        <section id="evidenza" class="evidence-section">
+            <div class="section py-5 pb-lg-80 px-lg-5 position-relative" style="<?php if (file_exists(get_stylesheet_directory().'/assets/img/evidenza-header.png')){ ?>background-image: url('<?php echo esc_url( get_stylesheet_directory_uri()); ?>/assets/img/evidenza-header.png');<?php }else{ ?>background-image: url('<?php echo esc_url( get_template_directory_uri()); ?>/assets/img/evidenza-header.png');<?php } ?>">
                 <?php get_template_part("template-parts/home/argomenti"); ?>
-                <?php get_template_part("template-parts/home/siti", "tematici"); ?>
+                <?php get_template_part("template-parts/home/siti","tematici"); ?>
             </div>
-        </div>
-
-    </section>
-    <?php get_template_part("template-parts/home/ricerca"); ?>
-    <?php get_template_part("template-parts/common/valuta-servizio"); ?>
-    <?php get_template_part("template-parts/common/assistenza-contatti"); ?>
-</main>
+        </section>
+        <?php get_template_part("template-parts/home/ricerca"); ?>
+        <?php get_template_part("template-parts/common/valuta-servizio"); ?>
+        <?php get_template_part("template-parts/common/assistenza-contatti"); ?>
+    </main>
 <?php
 get_footer();
