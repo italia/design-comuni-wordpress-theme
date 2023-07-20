ffb812836f5d405222cca9cccc6246e9d39f44cf
EM <e.manfredini@kalimera.it>
2023-07-20 18:40:00 +0200

    Archivi Argomenti e Cat. Servizio: template tassonomia
    - convertiti (rinominandoli) in template indipendenti, non inclusi da altri;
    - eliminato template taxonomy.php, superfluo.

--- taxonomy.php
+++
@@ -1,9 +0,0 @@
-<?php
-$obj = get_queried_object();
-
-if ($obj->taxonomy == "categorie_servizio")
-	get_template_part("archive-servizio");
-else if($obj->taxonomy == "argomenti")
-	get_template_part("archive-argomento");
-else
-	get_template_part("archive");
