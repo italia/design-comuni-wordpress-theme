fb3c0530ec77cc1d8aaa97644ee331c7558697ec
enrimk <93265448+enrimk@users.noreply.github.com>
2023-09-06 13:39:51 +0200

    refactor: archive template

--- taxonomy-argomenti.php
+++ taxonomy-argomenti.php
@@ -1,14 +1,16 @@
 <?php
 /**
- * The template for displaying archive
+ * Archivio Tassonomia Argomento
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#archive
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#custom-taxonomies
+ * @link https://italia.github.io/design-comuni-pagine-statiche/sito/argomento.html
  *
  * @package Design_Comuni_Italia
  */
-global $obj, $argomento, $with_border, $uo_id, $custom_class;
 
-$argomento = get_term_by('id', $obj->term_id, 'argomenti');
+global $argomento, $with_border, $uo_id, $custom_class;
+
+$argomento = get_queried_object();
 $img = dci_get_term_meta('immagine', "dci_term_", $argomento->term_id);
 $aree_appartenenza = dci_get_term_meta('area_appartenenza', "dci_term_", $argomento->term_id);
 $assessorati_riferimento = dci_get_term_meta('assessorato_riferimento', "dci_term_", $argomento->term_id);
