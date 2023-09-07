33a016f3168170c3dd4983d9c9c2a55f943a9b75
EM <e.manfredini@kalimera.it>
2023-09-08 00:01:13 +0200

    perf: get_tipologie_*
    array_column(), invece di un loop.
    Evita warning in caso di indici non definiti.

--- inc/comuni_config.php
+++ inc/comuni_config.php
@@ -54,11 +54,7 @@ function dci_get_tipologie_related_to_taxonomy($taxonomy) {
  * restituisce tutti i nomi delle tipologie del Sito dei Comuni
  */
 function dci_get_tipologie_names() {
-    $result = array();
-    foreach (COMUNI_TIPOLOGIE as $tipologia) {
-        $result[] = $tipologia['name'];
-    }
-    return $result;
+    return array_column(COMUNI_TIPOLOGIE, 'name');
 }
 
 /**
@@ -66,11 +62,7 @@ function dci_get_tipologie_names() {
  */
 
 function dci_get_tipologie_prefixes(){
-    $result = array();
-    foreach (COMUNI_TIPOLOGIE as $tipologia) {
-        $result[$tipologia['name']] = $tipologia['prefix'];
-    }
-    return $result;
+    return array_column(COMUNI_TIPOLOGIE, 'prefix', 'name');
 }
 
 
@@ -79,11 +71,7 @@ function dci_get_tipologie_prefixes(){
  * restituisce tuttle capability dei custom types del Sito dei Comuni
  */
 function dci_get_tipologie_capabilities(){
-    $result = array();
-    foreach (COMUNI_TIPOLOGIE as $tipologia) {
-        $result[] = $tipologia['capability'];
-    }
-    return $result;
+    return array_column(COMUNI_TIPOLOGIE, 'capability', 'name');
 }
 
 /**
