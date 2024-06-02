e0d0fcb5184f09f0164168234ecf4c67c6ed3353
Totolabs <info@totolabs.it>
2024-06-02 08:14:36 +0200

    Aggiunto il template "single-luogo", con la relativa card e corretta la card dell'unità organizzativa.

--- template-parts/unita-organizzativa/card.php
+++ template-parts/unita-organizzativa/card.php
@@ -4,42 +4,20 @@
 
     $prefix = '_dci_unita_organizzativa_';
     $img = dci_get_meta('immagine', $prefix, $uo_id);
-    $contatti = dci_get_meta('contatti', $prefix, $uo_id);
+	$sede_principale = dci_get_meta('sede_principale', $prefix, $uo_id);
+	$prefix = '_dci_luogo_';
+    $indirizzo = dci_get_meta('indirizzo', $prefix, $sede_principale);
 
-    $prefix = '_dci_punto_contatto_';
-    $indirizzi = array();
-    foreach ($contatti as $punto_contatto_id) {
-        $voci = dci_get_meta('voci', $prefix, $punto_contatto_id);
-        foreach ($voci as $voce) {
-            if ($voce[$prefix.'tipo_punto_contatto'] == 'indirizzo')
-                array_push($indirizzi, $voce[$prefix.'valore']);
-        }
+    $prefix = '_dci_luogo_';
+    $contatti = array();
+    foreach ($punti_contatto as $pc_id) {
+        $contatto = dci_get_full_punto_contatto($pc_id);
+        array_push($contatti, $contatto);
     }
     
     if($with_border) {
 ?>
-
-<div class="card card-teaser card-teaser-info rounded shadow-sm p-3">
-    <div class="card-body pe-3">
-        <p class="card-title text-paragraph-regular-medium-semi mb-3">
-            <a class="text-decoration-none" href="<?php echo get_permalink($ufficio->ID); ?>" data-element="service-area">
-            <?php echo $ufficio->post_title; ?>
-            </a>
-        </p>
-        <div class="card-text">
-            <?php foreach ($indirizzi as $indirizzo) {
-                echo '<p class="u-main-black">'.$indirizzo.'</p>';
-            }?>
-        </div>
-    </div>
-    <?php if ($img) { ?>
-        <div class="avatar size-xl">
-            <?php dci_get_img($img); ?>
-        </div>
-    <?php } ?>
-</div>
-<?php } else { ?>
-<div class="card card-teaser border rounded shadow p-4">
+<div class="card card-teaser border rounded shadow p-4 flex-nowrap">
     <div class="card-body pe-3">
         <h4 class="u-main-black mb-1 title-small-semi-bold-medium">
             <a class="text-decoration-none" href="<?php echo get_permalink($ufficio->ID); ?>">
@@ -47,9 +25,7 @@
             </a>
         </h4>
         <div class="card-text">
-            <?php foreach ($indirizzi as $indirizzo) {
-                echo '<p>'.$indirizzo.'</p>';
-            }?>
+            <p><?php echo $indirizzo; ?></p>
         </div>
     </div>
     <?php if ($img) { ?>
@@ -58,6 +34,29 @@
     </div>
     <?php } ?>
 </div>
+
+<?php } else { ?>
+
+<div class="card card-teaser card-teaser-info rounded shadow-sm p-3 flex-nowrap">
+    <div class="card-body pe-3">
+        <p class="card-title text-paragraph-regular-medium-semi mb-3">
+            <a class="text-decoration-none" href="<?php echo get_permalink($ufficio->ID); ?>" data-element="service-area">
+            <?php echo $ufficio->post_title; ?>
+            </a>
+        </p>
+        <div class="card-text">
+            <p class="u-main-black">
+				<?php echo $indirizzo;  ?>
+			</p>
+        </div>
+    </div>
+    <?php if ($img) { ?>
+        <div class="avatar size-xl">
+            <?php dci_get_img($img); ?>
+        </div>
+    <?php } ?>
+</div>
+
 <?php } 
 $with_border = false;
 ?>
