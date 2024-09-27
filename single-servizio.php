c4763bbd4871bb82b18756f10b4ce834cd0dde41
Francesco Improta <me@francescoimprota.com>
2024-09-27 15:43:42 +0200

    fix: move data-element
    Co-authored-by: enrimk <93265448+enrimk@users.noreply.github.com>

--- single-servizio.php
+++ single-servizio.php
@@ -405,9 +405,9 @@ get_header();
 
                             <section class="it-page-section">
                                 <h2 class="mb-3" id="contacts">Contatti</h2>
-                                <div class="row" data-element="service-area">
+                                <div class="row">
                                     <div class="col-12 col-md-8 col-lg-6 mb-30">
-                                        <div class="card-wrapper rounded h-auto mt-10">
+                                        <div class="card-wrapper rounded h-auto mt-10" data-element="service-area">
                                             <?php
                                                 $with_border = true;
                                                 get_template_part("template-parts/unita-organizzativa/card");
