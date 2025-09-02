ecc4ec0bbbf022e7c95bdbba478c5655425f52e0
Francesco Improta <me@francescoimprota.com>
2025-09-02 12:11:43 +0200

    fix: link to Figma resource

--- single.php
+++ single.php
@@ -56,7 +56,7 @@ get_header();
                                         <span class="text">Attenzione</span>
                                     </div>
                                     <p><strong>Questo è un sito demo e il template di questa pagina non è ancora disponibile.</strong></p>
-                                    <p>Controlla se è disponibile il <a href="https://www.figma.com/file/FHlE0r9lhfvDR0SgkDRmVi/Comuni---Modello-sito-e-servizi?type=design&node-id=1-1310&mode=design&t=YdmJ4xaUeMkCzJQc-0">layout su Figma</a>, il <a href="https://italia.github.io/design-comuni-pagine-statiche/">template HTML</a> tra le risorse del modello o consulta il <a href="https://designers.italia.it/files/resources/modelli/comuni/adotta-il-modello-di-sito-comunale/definisci-architettura-e-contenuti/Architettura-informazione-sito-Comuni.ods">documento di architettura (OSD 65kb)</a> per costruire il template in autonomia.</p>
+                                    <p>Controlla se è disponibile il <a href="https://www.figma.com/community/file/1262690210012419806/comuni-modello-sito-e-servizi">layout su Figma</a>, il <a href="https://italia.github.io/design-comuni-pagine-statiche/">template HTML</a> tra le risorse del modello o consulta il <a href="https://designers.italia.it/files/resources/modelli/comuni/adotta-il-modello-di-sito-comunale/definisci-architettura-e-contenuti/Architettura-informazione-sito-Comuni.ods">documento di architettura (OSD 65kb)</a> per costruire il template in autonomia.</p>
                                 </div>
                             </div>
                         </div>
